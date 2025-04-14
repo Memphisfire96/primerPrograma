e9b7b7b70f7f543bb9014bf0a3468e20d1a815b6
Memphis <kvn_araneda@hotmail.com>
2025-04-13 21:48:15 -0400

    trabajando con variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("bienvenido a Screen Match");
+        System.out.println("Pelicula : Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnELPlan = true;
+        double notaDeLaPelicula = 7.8;
+
+        double media = (notaDeLaPelicula + 6.0+ 9.0) / 3;
+        System.out.println(media);
+
+
+
+
+
+
     }
 }
