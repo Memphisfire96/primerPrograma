811f60e74c3727baacacaf7334b0b9f692560739
Memphis <kvn_araneda@hotmail.com>
2025-04-26 08:18:49 -0400

    Trabajo con operaciones

---
+++ src/Operaciones.java
@@ -0,0 +1,14 @@
+public class Operaciones {
+    public static void main(String[] args) {
+        int edad = 12;
+
+        if (edad>=18){
+            System.out.println("Tú eres mayor de edad");
+        }else{
+            System.out.println("eres menor de edad");
+        }
+
+
+
+    }
+}
