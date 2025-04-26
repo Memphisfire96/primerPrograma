bba0baa0db211d63edeed5611d7d9064dc648284
Memphis <kvn_araneda@hotmail.com>
2025-04-26 08:19:09 -0400

    Desafio de Conversion

---
+++ src/Temperatura.java
@@ -0,0 +1,12 @@
+public class Temperatura {
+
+    public static void main(String[] args) {
+        double temperatura = 30.3;
+        int conversion = (int) ((temperatura * 1.8)+32);
+        System.out.println("La temperatura en grados celsius es de: " + temperatura);
+        System.out.println("la temperatura en grados farenheit es de: "+conversion);
+
+
+
+    }
+}
